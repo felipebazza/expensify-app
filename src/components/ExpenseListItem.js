e6e1b090147daba365ec43f582693bb3923995ea
Felipe Bazzanella <felipe.dracco@gmail.com>
2019-08-08 15:15:00 -0300

    lesson-106

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-export default ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
     <h3>Description: {description}</h3>
     <p>Amount: {amount}</p>
     <p>Created At: {createdAt}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }));
+    }} >Remove</button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
