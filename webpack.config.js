6108eaa55e5d58ed40526e9d0dc9041e7bc3e54d
Felipe Bazzanella <felipe.dracco@gmail.com>
2024-01-12 16:13:58 -0300

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,20 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env) => {
     const isProduction = env === 'production';
-
+    
     return {
         entry: './src/index.js',
         output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'bundle.js'
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js',
+            publicPath: '/dist/'
         },
         plugins: [
             new MiniCssExtractPlugin({
                 filename: "styles.css",
             }),
         ],
+        mode: 'development',
         module: {
             rules: [{
                 loader: 'babel-loader',
@@ -42,7 +44,7 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             static: {
-                directory: path.join(__dirname, 'public'),
+                directory: path.join(__dirname, 'public')
             },
             historyApiFallback: true
         }
